a29a789259f342c78e75fd5f99dde18173564d43
Boba Fett <thugfencer@gmail.com>
2022-04-18 19:40:07 -0700

    Update comments for Taco Truck

--- M02/src/com/olympic/cis143/m02/student/tacotruck/queue/OrdersQueueImpl.java
+++ M02/src/com/olympic/cis143/m02/student/tacotruck/queue/OrdersQueueImpl.java
@@ -9,18 +9,22 @@ public class OrdersQueueImpl {
 
 	private Deque<TacoImpl> tacoQueue = new ArrayDeque<>();
     
+	// Add an order to the queue
     public void addOrder(TacoImpl tacoOrder) {
     	tacoQueue.add(tacoOrder);							
     }
     
+    // Check if queue is empty. True if more tacos remain, else false.
     public boolean hasNext() {
     	return tacoQueue.isEmpty() ? false : true;
     }
     
+    // First order completed, remove it from the queue and return it
     public TacoImpl closeNextOrder() {
         return tacoQueue.removeFirst();						
     }
     
+    // Return number of orders on the queue
     public int howManyOrders() {
         return tacoQueue.size();							
     }
