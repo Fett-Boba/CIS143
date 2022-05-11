bc1387c8be25749664925df997a79e72db9b75a7
Boba Fett <thugfencer@gmail.com>
2022-05-10 23:14:01 -0700

    Rick Rolled - Never Gonna

---
+++ Codewars/src/com/boba/practiceproblems/NeverGonna.java
@@ -0,0 +1,32 @@
+package com.boba.practiceproblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NeverGonna {
+
+	  public static List<String> music(List<Integer> numbers){
+		
+		  List <String> chorus = new ArrayList<>();
+		  chorus.add("Never gonna give you up");
+		  chorus.add("Never gonna let you down");
+		  chorus.add("Never gonna run around and desert you");
+		  chorus.add("Never gonna make you cry");
+		  chorus.add("Never gonna say goodbye");
+		  chorus.add("Never gonna tell a lie and hurt you");
+		  
+		  List <String> newChorus = new ArrayList<>();
+		  String str = "";
+		  
+		  for (int i = 0; i < numbers.size(); i++) {
+			  if (i % 2 == 0 ) {
+				  newChorus.add(chorus.get(numbers.get(i)));
+			  } else {
+				  str = chorus.get(numbers.get(i));
+				  newChorus.add(str.replace("Never gonna", "NEVER GONNA"));
+			  }
+		  }
+		  return newChorus;
+	  } 
+	
+}
