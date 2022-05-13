eb45687863887055d0ba855ac32807b54fdf3e28
Boba Fett <thugfencer@gmail.com>
2022-05-12 22:42:51 -0700

    ISBN10 to 13 and bit counting

---
+++ Codewars/src/com/boba/practiceproblems/BitCountingTest.java
@@ -0,0 +1,18 @@
+package com.boba.practiceproblems;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BitCountingTest {
+
+	  @Test
+	  public void testGame() {
+	    assertEquals(5, BitCounting.countBits(1234)); 
+	    assertEquals(1, BitCounting.countBits(4)); 
+	    assertEquals(3, BitCounting.countBits(7)); 
+	    assertEquals(2, BitCounting.countBits(9)); 
+	    assertEquals(2, BitCounting.countBits(10)); 
+	  }
+
+}
