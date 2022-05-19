fcd6bc97bae6c49f923bb45a76b6dbc71f7fc5de
Boba Fett <thugfencer@gmail.com>
2022-05-19 11:49:29 -0700

    Update the score grid

--- Touche/src/com/olympic/cis143/student/touche/gui/Touche.java
+++ Touche/src/com/olympic/cis143/student/touche/gui/Touche.java
@@ -19,11 +19,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.SwingConstants;
 
 public class Touche {
@@ -40,6 +35,70 @@ public class Touche {
 	private JTextField txtFencer7;
 	private JTextField txtFencer8;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
+	private JTextField textField_14;
+	private JTextField textField_15;
+	private JTextField textField_16;
+	private JTextField textField_17;
+	private JTextField textField_18;
+	private JTextField textField_19;
+	private JTextField textField_20;
+	private JTextField textField_21;
+	private JTextField textField_22;
+	private JTextField textField_23;
+	private JTextField textField_24;
+	private JTextField textField_25;
+	private JTextField textField_26;
+	private JTextField textField_27;
+	private JTextField textField_28;
+	private JTextField textField_29;
+	private JTextField textField_30;
+	private JTextField textField_31;
+	private JTextField textField_32;
+	private JTextField textField_33;
+	private JTextField textField_34;
+	private JTextField textField_35;
+	private JTextField textField_36;
+	private JTextField textField_37;
+	private JTextField textField_38;
+	private JTextField textField_39;
+	private JTextField textField_40;
+	private JTextField textField_41;
+	private JTextField textField_42;
+	private JTextField textField_43;
+	private JTextField textField_44;
+	private JTextField textField_45;
+	private JTextField textField_46;
+	private JTextField textField_47;
+	private JTextField textField_48;
+	private JTextField textField_49;
+	private JTextField textField_50;
+	private JTextField textField_51;
+	private JTextField textField_52;
+	private JTextField textField_53;
+	private JTextField textField_54;
+	private JTextField textField_55;
+	private JTextField textField_56;
+	private JTextField textField_57;
+	private JTextField textField_58;
+	private JTextField textField_59;
+	private JTextField textField_60;
+	private JTextField textField_61;
+	private JTextField textField_62;
+	private JTextField textField_63;
 
 	/**
 	 * Launch the application.
@@ -300,34 +359,29 @@ public class Touche {
 		btnScoresExit.setBounds(398, 511, 89, 23);
 		pnlScores.add(btnScoresExit);
 		
-		JLabel lblScoresName = new JLabel("NAME");
-		lblScoresName.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresName.setBounds(49, 113, 125, 14);
-		pnlScores.add(lblScoresName);
-		
 		JLabel lblScoresFencer1 = new JLabel("New label");
 		lblScoresFencer1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer1.setBounds(49, 148, 125, 14);
+		lblScoresFencer1.setBounds(49, 153, 125, 14);
 		pnlScores.add(lblScoresFencer1);
 		
 		JLabel lblScoresFencer2 = new JLabel("New label");
 		lblScoresFencer2.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer2.setBounds(49, 183, 125, 14);
+		lblScoresFencer2.setBounds(49, 187, 125, 14);
 		pnlScores.add(lblScoresFencer2);
 		
 		JLabel lblScoresFencer3 = new JLabel("New label");
 		lblScoresFencer3.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer3.setBounds(49, 218, 125, 14);
+		lblScoresFencer3.setBounds(49, 221, 125, 14);
 		pnlScores.add(lblScoresFencer3);
 		
 		JLabel lblScoresFencer4 = new JLabel("New label");
 		lblScoresFencer4.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer4.setBounds(49, 253, 125, 14);
+		lblScoresFencer4.setBounds(49, 255, 125, 14);
 		pnlScores.add(lblScoresFencer4);
 		
 		JLabel lblScoresFencer5 = new JLabel("New label");
 		lblScoresFencer5.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer5.setBounds(49, 288, 125, 14);
+		lblScoresFencer5.setBounds(49, 289, 125, 14);
 		pnlScores.add(lblScoresFencer5);
 		
 		JLabel lblScoresFencer6 = new JLabel("New label");
@@ -337,13 +391,413 @@ public class Touche {
 		
 		JLabel lblScoresFencer7 = new JLabel("New label");
 		lblScoresFencer7.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer7.setBounds(49, 358, 125, 14);
+		lblScoresFencer7.setBounds(49, 357, 125, 14);
 		pnlScores.add(lblScoresFencer7);
 		
 		JLabel lblScoresFencer8 = new JLabel("New label");
 		lblScoresFencer8.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblScoresFencer8.setBounds(49, 393, 125, 14);
+		lblScoresFencer8.setBounds(49, 391, 125, 14);
 		pnlScores.add(lblScoresFencer8);
+		
+		JLabel lblNewLabel_1 = new JLabel("1");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(184, 153, 26, 14);
+		pnlScores.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("2");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_1.setBounds(184, 187, 26, 14);
+		pnlScores.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("3");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_2.setBounds(184, 221, 26, 14);
+		pnlScores.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("4");
+		lblNewLabel_1_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_3.setBounds(184, 255, 26, 14);
+		pnlScores.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("5");
+		lblNewLabel_1_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_4.setBounds(184, 289, 26, 14);
+		pnlScores.add(lblNewLabel_1_4);
+		
+		JLabel lblNewLabel_1_5 = new JLabel("6");
+		lblNewLabel_1_5.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_5.setBounds(184, 323, 26, 14);
+		pnlScores.add(lblNewLabel_1_5);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("7");
+		lblNewLabel_1_6.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_6.setBounds(184, 357, 26, 14);
+		pnlScores.add(lblNewLabel_1_6);
+		
+		JLabel lblNewLabel_1_7 = new JLabel("8");
+		lblNewLabel_1_7.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_7.setBounds(184, 391, 26, 14);
+		pnlScores.add(lblNewLabel_1_7);
+		
+		JLabel lblNewLabel_1_8 = new JLabel("1");
+		lblNewLabel_1_8.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_8.setBounds(225, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_8);
+		
+		JLabel lblNewLabel_1_9 = new JLabel("2");
+		lblNewLabel_1_9.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_9.setBounds(265, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_9);
+		
+		JLabel lblNewLabel_1_10 = new JLabel("3");
+		lblNewLabel_1_10.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_10.setBounds(305, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_10);
+		
+		JLabel lblNewLabel_1_11 = new JLabel("4");
+		lblNewLabel_1_11.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_11.setBounds(345, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_11);
+		
+		JLabel lblNewLabel_1_12 = new JLabel("5");
+		lblNewLabel_1_12.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_12.setBounds(385, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_12);
+		
+		JLabel lblNewLabel_1_13 = new JLabel("6");
+		lblNewLabel_1_13.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_13.setBounds(425, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_13);
+		
+		JLabel lblNewLabel_1_14 = new JLabel("7");
+		lblNewLabel_1_14.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_14.setBounds(465, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_14);
+		
+		JLabel lblNewLabel_1_15 = new JLabel("8");
+		lblNewLabel_1_15.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1_15.setBounds(505, 126, 26, 14);
+		pnlScores.add(lblNewLabel_1_15);
+		
+		textField = new JTextField();
+		textField.setBounds(220, 145, 36, 30);
+		pnlScores.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(260, 145, 36, 30);
+		pnlScores.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(300, 145, 36, 30);
+		pnlScores.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(340, 145, 36, 30);
+		pnlScores.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(380, 145, 36, 30);
+		pnlScores.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(420, 145, 36, 30);
+		pnlScores.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(460, 145, 36, 30);
+		pnlScores.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(500, 145, 36, 30);
+		pnlScores.add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(220, 179, 36, 30);
+		pnlScores.add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(260, 179, 36, 30);
+		pnlScores.add(textField_9);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		textField_10.setBounds(300, 179, 36, 30);
+		pnlScores.add(textField_10);
+		
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		textField_11.setBounds(340, 179, 36, 30);
+		pnlScores.add(textField_11);
+		
+		textField_12 = new JTextField();
+		textField_12.setColumns(10);
+		textField_12.setBounds(380, 179, 36, 30);
+		pnlScores.add(textField_12);
+		
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		textField_13.setBounds(420, 179, 36, 30);
+		pnlScores.add(textField_13);
+		
+		textField_14 = new JTextField();
+		textField_14.setColumns(10);
+		textField_14.setBounds(460, 179, 36, 30);
+		pnlScores.add(textField_14);
+		
+		textField_15 = new JTextField();
+		textField_15.setColumns(10);
+		textField_15.setBounds(500, 179, 36, 30);
+		pnlScores.add(textField_15);
+		
+		textField_16 = new JTextField();
+		textField_16.setColumns(10);
+		textField_16.setBounds(220, 213, 36, 30);
+		pnlScores.add(textField_16);
+		
+		textField_17 = new JTextField();
+		textField_17.setColumns(10);
+		textField_17.setBounds(260, 213, 36, 30);
+		pnlScores.add(textField_17);
+		
+		textField_18 = new JTextField();
+		textField_18.setColumns(10);
+		textField_18.setBounds(300, 213, 36, 30);
+		pnlScores.add(textField_18);
+		
+		textField_19 = new JTextField();
+		textField_19.setColumns(10);
+		textField_19.setBounds(340, 213, 36, 30);
+		pnlScores.add(textField_19);
+		
+		textField_20 = new JTextField();
+		textField_20.setColumns(10);
+		textField_20.setBounds(380, 213, 36, 30);
+		pnlScores.add(textField_20);
+		
+		textField_21 = new JTextField();
+		textField_21.setColumns(10);
+		textField_21.setBounds(420, 213, 36, 30);
+		pnlScores.add(textField_21);
+		
+		textField_22 = new JTextField();
+		textField_22.setColumns(10);
+		textField_22.setBounds(460, 213, 36, 30);
+		pnlScores.add(textField_22);
+		
+		textField_23 = new JTextField();
+		textField_23.setColumns(10);
+		textField_23.setBounds(500, 213, 36, 30);
+		pnlScores.add(textField_23);
+		
+		textField_24 = new JTextField();
+		textField_24.setColumns(10);
+		textField_24.setBounds(220, 247, 36, 30);
+		pnlScores.add(textField_24);
+		
+		textField_25 = new JTextField();
+		textField_25.setColumns(10);
+		textField_25.setBounds(260, 247, 36, 30);
+		pnlScores.add(textField_25);
+		
+		textField_26 = new JTextField();
+		textField_26.setColumns(10);
+		textField_26.setBounds(300, 247, 36, 30);
+		pnlScores.add(textField_26);
+		
+		textField_27 = new JTextField();
+		textField_27.setColumns(10);
+		textField_27.setBounds(340, 247, 36, 30);
+		pnlScores.add(textField_27);
+		
+		textField_28 = new JTextField();
+		textField_28.setColumns(10);
+		textField_28.setBounds(380, 247, 36, 30);
+		pnlScores.add(textField_28);
+		
+		textField_29 = new JTextField();
+		textField_29.setColumns(10);
+		textField_29.setBounds(420, 247, 36, 30);
+		pnlScores.add(textField_29);
+		
+		textField_30 = new JTextField();
+		textField_30.setColumns(10);
+		textField_30.setBounds(460, 247, 36, 30);
+		pnlScores.add(textField_30);
+		
+		textField_31 = new JTextField();
+		textField_31.setColumns(10);
+		textField_31.setBounds(500, 247, 36, 30);
+		pnlScores.add(textField_31);
+		
+		textField_32 = new JTextField();
+		textField_32.setColumns(10);
+		textField_32.setBounds(220, 281, 36, 30);
+		pnlScores.add(textField_32);
+		
+		textField_33 = new JTextField();
+		textField_33.setColumns(10);
+		textField_33.setBounds(260, 281, 36, 30);
+		pnlScores.add(textField_33);
+		
+		textField_34 = new JTextField();
+		textField_34.setColumns(10);
+		textField_34.setBounds(300, 281, 36, 30);
+		pnlScores.add(textField_34);
+		
+		textField_35 = new JTextField();
+		textField_35.setColumns(10);
+		textField_35.setBounds(340, 281, 36, 30);
+		pnlScores.add(textField_35);
+		
+		textField_36 = new JTextField();
+		textField_36.setColumns(10);
+		textField_36.setBounds(380, 281, 36, 30);
+		pnlScores.add(textField_36);
+		
+		textField_37 = new JTextField();
+		textField_37.setColumns(10);
+		textField_37.setBounds(420, 281, 36, 30);
+		pnlScores.add(textField_37);
+		
+		textField_38 = new JTextField();
+		textField_38.setColumns(10);
+		textField_38.setBounds(460, 281, 36, 30);
+		pnlScores.add(textField_38);
+		
+		textField_39 = new JTextField();
+		textField_39.setColumns(10);
+		textField_39.setBounds(500, 281, 36, 30);
+		pnlScores.add(textField_39);
+		
+		textField_40 = new JTextField();
+		textField_40.setColumns(10);
+		textField_40.setBounds(220, 315, 36, 30);
+		pnlScores.add(textField_40);
+		
+		textField_41 = new JTextField();
+		textField_41.setColumns(10);
+		textField_41.setBounds(260, 315, 36, 30);
+		pnlScores.add(textField_41);
+		
+		textField_42 = new JTextField();
+		textField_42.setColumns(10);
+		textField_42.setBounds(300, 315, 36, 30);
+		pnlScores.add(textField_42);
+		
+		textField_43 = new JTextField();
+		textField_43.setColumns(10);
+		textField_43.setBounds(340, 315, 36, 30);
+		pnlScores.add(textField_43);
+		
+		textField_44 = new JTextField();
+		textField_44.setColumns(10);
+		textField_44.setBounds(380, 315, 36, 30);
+		pnlScores.add(textField_44);
+		
+		textField_45 = new JTextField();
+		textField_45.setColumns(10);
+		textField_45.setBounds(420, 315, 36, 30);
+		pnlScores.add(textField_45);
+		
+		textField_46 = new JTextField();
+		textField_46.setColumns(10);
+		textField_46.setBounds(460, 315, 36, 30);
+		pnlScores.add(textField_46);
+		
+		textField_47 = new JTextField();
+		textField_47.setColumns(10);
+		textField_47.setBounds(500, 315, 36, 30);
+		pnlScores.add(textField_47);
+		
+		textField_48 = new JTextField();
+		textField_48.setColumns(10);
+		textField_48.setBounds(220, 349, 36, 30);
+		pnlScores.add(textField_48);
+		
+		textField_49 = new JTextField();
+		textField_49.setColumns(10);
+		textField_49.setBounds(260, 349, 36, 30);
+		pnlScores.add(textField_49);
+		
+		textField_50 = new JTextField();
+		textField_50.setColumns(10);
+		textField_50.setBounds(300, 349, 36, 30);
+		pnlScores.add(textField_50);
+		
+		textField_51 = new JTextField();
+		textField_51.setColumns(10);
+		textField_51.setBounds(340, 349, 36, 30);
+		pnlScores.add(textField_51);
+		
+		textField_52 = new JTextField();
+		textField_52.setColumns(10);
+		textField_52.setBounds(380, 349, 36, 30);
+		pnlScores.add(textField_52);
+		
+		textField_53 = new JTextField();
+		textField_53.setColumns(10);
+		textField_53.setBounds(420, 349, 36, 30);
+		pnlScores.add(textField_53);
+		
+		textField_54 = new JTextField();
+		textField_54.setColumns(10);
+		textField_54.setBounds(460, 349, 36, 30);
+		pnlScores.add(textField_54);
+		
+		textField_55 = new JTextField();
+		textField_55.setColumns(10);
+		textField_55.setBounds(500, 349, 36, 30);
+		pnlScores.add(textField_55);
+		
+		textField_56 = new JTextField();
+		textField_56.setColumns(10);
+		textField_56.setBounds(220, 383, 36, 30);
+		pnlScores.add(textField_56);
+		
+		textField_57 = new JTextField();
+		textField_57.setColumns(10);
+		textField_57.setBounds(260, 383, 36, 30);
+		pnlScores.add(textField_57);
+		
+		textField_58 = new JTextField();
+		textField_58.setColumns(10);
+		textField_58.setBounds(300, 383, 36, 30);
+		pnlScores.add(textField_58);
+		
+		textField_59 = new JTextField();
+		textField_59.setColumns(10);
+		textField_59.setBounds(340, 383, 36, 30);
+		pnlScores.add(textField_59);
+		
+		textField_60 = new JTextField();
+		textField_60.setColumns(10);
+		textField_60.setBounds(380, 383, 36, 30);
+		pnlScores.add(textField_60);
+		
+		textField_61 = new JTextField();
+		textField_61.setColumns(10);
+		textField_61.setBounds(420, 383, 36, 30);
+		pnlScores.add(textField_61);
+		
+		textField_62 = new JTextField();
+		textField_62.setColumns(10);
+		textField_62.setBounds(460, 383, 36, 30);
+		pnlScores.add(textField_62);
+		
+		textField_63 = new JTextField();
+		textField_63.setColumns(10);
+		textField_63.setBounds(500, 383, 36, 30);
+		pnlScores.add(textField_63);
 		
 		
 
@@ -432,7 +886,7 @@ public class Touche {
 					!chckbxFencer8.isSelected() ) {
 					JOptionPane.showMessageDialog(frame, "Please select which fencer is you.", "Error", JOptionPane.ERROR_MESSAGE);
 				} else {
-					// If user did not enter any names default them
+					// If user did not enter names default them
 					if (txtFencer1.getText().isEmpty()) txtFencer1.setText("Fencer1");
 					if (txtFencer2.getText().isEmpty()) txtFencer2.setText("Fencer2");
 					if (txtFencer3.getText().isEmpty()) txtFencer3.setText("Fencer3");
