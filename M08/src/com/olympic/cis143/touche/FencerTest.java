d0ce7f3cfe59494a405e6376146c1c9f85ef1894
Boba Fett <thugfencer@gmail.com>
2022-06-12 18:59:21 -0700

    Streamline test to initialize array list data all at once.

--- M08/src/com/olympic/cis143/touche/FencerTest.java
+++ M08/src/com/olympic/cis143/touche/FencerTest.java
@@ -3,6 +3,7 @@ package com.olympic.cis143.touche;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
@@ -15,19 +16,9 @@ class FencerTest {
 	
 	@Test
 	void testCalculateVictories() {
-		
 		Fencer f1 = new Fencer();
-		
-		List <Integer> ts = new ArrayList<>();
-		ts.add(5);
-		ts.add(2);
-		ts.add(5);
-
-		List <Integer> tr = new ArrayList<>();
-		tr.add(1);
-		tr.add(5);
-		tr.add(0);
-		
+		List <Integer> ts = new ArrayList<>(Arrays.asList(5,2,5));
+		List <Integer> tr = new ArrayList<>(Arrays.asList(1,5,0));
 		f1.setAlTs(ts);
 		f1.setAlTr(tr);
 		assertEquals(2, f1.calculateVictories());
@@ -35,46 +26,25 @@ class FencerTest {
 
 	@Test
 	void testCalculateTouchesScored() {
-		
 		Fencer f2 = new Fencer();
-		List <Integer> ts = new ArrayList<>();
-		ts.add(5);
-		ts.add(2);
-		ts.add(5);
-		
+		List <Integer> ts = new ArrayList<>(Arrays.asList(5,2,5));
 		f2.setAlTs(ts);
 		assertEquals(12, f2.calculateTouchesScored());
 	}
 
 	@Test
 	void testCalculateTouchesReceived() {
-		
 		Fencer f3 = new Fencer();
-
-		List <Integer> tr = new ArrayList<>();
-		tr.add(1);
-		tr.add(5);
-		tr.add(0);
-
+		List <Integer> tr = new ArrayList<>(Arrays.asList(1,5,0));
 		f3.setAlTr(tr);
 		assertEquals(6, f3.calculateTouchesReceived());
 	}
 
 	@Test
 	void testCalculateIndicator() {
-		
 		Fencer f4 = new Fencer();
-
-		List <Integer> ts = new ArrayList<>();
-		ts.add(5);
-		ts.add(2);
-		ts.add(5);
-
-		List <Integer> tr = new ArrayList<>();
-		tr.add(1);
-		tr.add(5);
-		tr.add(0);
-		
+		List <Integer> ts = new ArrayList<>(Arrays.asList(5,2,5));
+		List <Integer> tr = new ArrayList<>(Arrays.asList(1,5,0));		
 		f4.setAlTs(ts);
 		f4.setAlTr(tr);
 		assertEquals(6, f4.calculateIndicator());
@@ -82,18 +52,9 @@ class FencerTest {
 
 	@Test
 	void testToString() {
-
 		Fencer f5 = new Fencer();
-
-		List <Integer> ts = new ArrayList<>();
-		ts.add(5);
-		ts.add(2);
-		ts.add(5);
-		List <Integer> tr = new ArrayList<>();
-		tr.add(1);
-		tr.add(5);
-		tr.add(0);
-
+		List <Integer> ts = new ArrayList<>(Arrays.asList(5,2,5));
+		List <Integer> tr = new ArrayList<>(Arrays.asList(1,5,0));
 		f5.setFencerNum(3);
 		f5.setName("Dread Pirate Roberts");
 		f5.setAlTs(ts);
@@ -103,9 +64,6 @@ class FencerTest {
 		f5.setVictories(2);
 		f5.setIndicator(6);
 		f5.setPlace(2);
-
 		assertEquals("Fencer [fencerNum=3, name=Dread Pirate Roberts, alTs=[5, 2, 5], alTr=[1, 5, 0], victories=2, touchesScored=12, touchesReceived=6, indicator=6, place=2]", f5.toString());
-		
 	}
-
 }
